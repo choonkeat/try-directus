bb6d81b0571cdce1c5d29cc61bdbaddcf7d06080
choonkeat <choonkeat@gmail.com>
2024-11-24 07:47:58 +0800

    compatible endpoint extension

--- Makefile
+++ Makefile
@@ -16,6 +16,6 @@ build-myhook:
 build-mybundle:
 	(cd mybundle && npm run build)
 
-EXTENSION=myendpoint
+EXTENSION=directus-extension-myendpoint
 try-myendpoint:
 	curl -i http://localhost:8055/$(EXTENSION)/
--- myendpoint/src/index.js
+++ myendpoint/src/index.js
@@ -1,9 +1,12 @@
-export default {
-    id: 'myendpoint',
-    handler: (router) => {
-        router.get('/', (req, res) => {
-            console.log("[console.log] Hello, World!\n");
-            res.send("Hello, World!\n")
-        });
-    }
+export default (router) => {
+    router.get('/', ({ url, headers, body }, res) => {
+        let responseBody = `Hello, ${JSON.stringify({ url, headers, body })} !\n`
+        console.log(`[console.log v1] ${responseBody}`);
+
+        if (res && typeof res.send === 'function') {
+            return res.send(`res.send: ${responseBody}`);
+        } else {
+            return { status: 200, body: `return: ${responseBody}` };
+        }
+    });
 };
