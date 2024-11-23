427fcb6ddfcc6b55f7a521d45a7afebc0db455c7
choonkeat <choonkeat@gmail.com>
2024-11-24 07:53:32 +0800

    sandboxed endpoint extension

--- myendpoint/src/index.js
+++ myendpoint/src/index.js
@@ -1,12 +1,14 @@
+import { log } from 'directus:api';
+
 export default (router) => {
     router.get('/', ({ url, headers, body }, res) => {
         let responseBody = `Hello, ${JSON.stringify({ url, headers, body })} !\n`
-        console.log(`[console.log v1] ${responseBody}`);
+        log(`[sandboxed v1] ${responseBody}`);
 
         if (res && typeof res.send === 'function') {
-            return res.send(`res.send: ${responseBody}`);
+            return res.send(`res.send: ${responseBody}`); // non-sandbox
         } else {
-            return { status: 200, body: `return: ${responseBody}` };
+            return { status: 200, body: `return: ${responseBody}` }; // sandbox
         }
     });
 };
